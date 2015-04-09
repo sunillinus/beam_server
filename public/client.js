09abf4a352cdc3c4ebe5c843e69ff309e05af39c
sunillinus <sunil.linus@gmail.com>
2015-04-09 00:10:21 -0700

    First commit

---
+++ public/client.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  var server = io.connect('http://localhost:3000');
+  server.on('message', function(data) {
+    console.log('client received: ' + JSON.stringify(data));
+    var html = '';
+    if(data.type == 'html') {
+      html = data.data;
+    } else if (data.type == 'image') {
+      html = '<img class="img-responsive" src="' + data.data + '"/>';      
+    } else if (data.type == 'link') {
+      html = '<a href="' + data.data + '">Link</a>';
+    } else if (data.type == 'selection') {
+      html = data.data;
+    } else if (data.type == 'page') {
+      html = '<iframe type="text/html" src="' + data.data
+       + '" frameborder="0" allowfullscreen="" webkitallowfullscreen=""></iframe>';
+    }
+    if (html) insertContent(html);
+    // $('#insert-here').html(html);
+  });
+});
+
+var insertContent = function(html) {
+  console.log('insert:' + html);
+  var template = _.template($("#content-template").html());
+  var x = template({html: html});
+  // console.log(x);
+  $('#target').prepend(x);
+};
---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var socket = require('socket.io');
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({ extended: false });
+var client = null;
+
+app.use('/', express.static('public'));
+
+// beam
+app.post('/beam', parseUrlencoded, function(request, response) {
+  console.log('Beam: ' + JSON.stringify(request.body));
+  io.sockets.emit('message', request.body);
+  response.sendStatus(200);
+});
+
+// http
+var server = app.listen(3000, function() {
+  console.log('Listening on 3000...')
+});
+
+var io = socket.listen(server);
+io.on('connection', function(client) { // io.sockets.on(...) also works?
+  console.log('Client connected...');
+  client.emit('message', {message: 'Hello'});
+
+  // send heart beat
+  // setInterval(function() {
+  //   data = { value: Math.floor(Math.random()*100), created_at: Date.now() }
+  //   client.emit('messages', data);
+  // }, 5000);
+});
